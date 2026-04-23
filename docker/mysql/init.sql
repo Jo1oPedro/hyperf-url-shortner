b4e29bd8ad0c085bc127f453eb494a1f084e91e7
Jo1o Pedro <joao.pedreira@estudante.ufjf.br>
2026-04-23 20:10:18 -0300

    chore(sql): add init sql for test database

---
+++ docker/mysql/init.sql
@@ -0,0 +1,3 @@
+CREATE DATABASE IF NOT EXISTS hyperf_url_shortner_test CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+GRANT ALL PRIVILEGES ON hyperf_url_shortner_test.* TO 'hyperf'@'%';
+FLUSH PRIVILEGES;
