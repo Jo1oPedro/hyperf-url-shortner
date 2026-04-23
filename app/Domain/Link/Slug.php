08312181a55df113e29aebc7ebf8381173db7637
Jo1o Pedro <joao.pedreira@estudante.ufjf.br>
2026-04-23 15:53:55 -0300

    Add random slug generation with tests

--- app/Domain/Link/Slug.php
+++ app/Domain/Link/Slug.php
@@ -16,6 +16,23 @@ readonly class Slug
 
     public function __toString(): string
     {
+        return $this->slug;
+    }
 
+    public static function random(int $length = 7): self
+    {
+        if($length < 4 || $length > 16) {
+            throw new \InvalidArgumentException("Slug length must be between 4 and 16");
+        }
+
+        $characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_-';
+        $maxIndex = strlen($characters) - 1;
+        $slug = "";
+
+        for ($i = 0; $i < $length; $i++) {
+            $slug .= $characters[random_int(0, $maxIndex)];
+        }
+
+        return new self($slug);
     }
 }
--- test/Unit/Domain/Link/SlugTest.php
+++ test/Unit/Domain/Link/SlugTest.php
@@ -30,4 +30,35 @@ class SlugTest extends TestCase
 
         new Slug('slug@invalido');
     }
+
+    public function test_rejeita_random_slug_muito_curto(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+
+        Slug::random(3);
+    }
+
+    public function test_rejeita_random_slug_muito_longo(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+
+        Slug::random(17);
+    }
+
+    public function test_aceita_random_slug_sem_tamanho_informado_valido(): void
+    {
+        $slug = Slug::random();
+
+        $this->assertInstanceOf(Slug::class, $slug);
+        $this->assertSame(7, strlen((string) $slug));
+    }
+
+    public function test_aceita_random_slug_com_tamanho_informado_valido(): void
+    {
+        $length = 12;
+        $slug = Slug::random($length);
+
+        $this->assertInstanceOf(Slug::class, $slug);
+        $this->assertSame($length, strlen((string) $slug));
+    }
 }
