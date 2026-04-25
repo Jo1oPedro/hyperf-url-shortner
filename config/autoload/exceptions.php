df792b8dae467b76120ccb6b933f29c9faa992e0
Jo1o Pedro <joao.pedreira@estudante.ufjf.br>
2026-04-25 20:10:22 -0300

    config(link): add testing rethrow, link gone and link not found exception handlers

--- config/autoload/exceptions.php
+++ config/autoload/exceptions.php
@@ -10,15 +10,21 @@ declare(strict_types=1);
  * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
  */
 
+use App\Exception\Handler\LinkGoneHandler;
+use App\Exception\Handler\LinkNotFoundHandler;
 use App\Exception\Handler\SlugAlreadyExistsHandler;
+use App\Exception\Handler\TestingRethrowHandler;
 use Hyperf\Validation\ValidationExceptionHandler;
 
 return [
     'handler' => [
         'http' => [
+            TestingRethrowHandler::class,
             Hyperf\HttpServer\Exception\Handler\HttpExceptionHandler::class,
             ValidationExceptionHandler::class,
             SlugAlreadyExistsHandler::class,
+            LinkGoneHandler::class,
+            LinkNotFoundHandler::class,
             App\Exception\Handler\AppExceptionHandler::class,
         ],
     ],
