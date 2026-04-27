43e9862ad39d46a2b2d9926327226e8974fd3b59
Jo1o Pedro <joao.pedreira@estudante.ufjf.br>
2026-04-26 23:49:13 -0300

    test(link): add test to validate that second request with unknown slug does not hit the database

--- test/Feature/Link/RedirectTest.php
+++ test/Feature/Link/RedirectTest.php
@@ -2,9 +2,17 @@
 
 namespace HyperfTest\Feature\Link;
 
+use App\Controller\LinkController;
+use App\Domain\Link\LinkRepository;
 use App\Domain\Link\LinkStatus;
+use App\Domain\Link\SlugGenerator;
+use App\Infrastructure\Link\EloquentLinkRepository;
 use App\Model\Link;
+use App\Service\LinkCache;
+use App\Service\LinkService;
+use Hyperf\Context\ApplicationContext;
 use HyperfTest\HttpTestCase;
+use function Hyperf\Tappable\tap;
 
 class RedirectTest extends HttpTestCase
 {
@@ -57,5 +65,48 @@ class RedirectTest extends HttpTestCase
         $response = $this->get("/inativo");
 
         $response->assertStatus(410);
+    }
+
+    public function test_segundo_request_nao_consulta_banco(): void
+    {
+        $container = ApplicationContext::getContainer();
+        $linkCache = $container->get(LinkCache::class);
+        $linkCache->invalidate("cached");
+
+        $mockRepo = $this->createMock(LinkRepository::class);
+        $mockRepo->expects($this->once())
+            ->method("findBySlug")
+            ->willReturn(tap(new Link(), function (Link $link) {
+                $link->slug = "cached";
+                $link->original_url = "https://example.com";
+                $link->status = LinkStatus::ACTIVE->value;
+                $link->expires_at = null;
+            }));
+
+        $mockService = new LinkService(
+            $mockRepo,
+            $container->get(SlugGenerator::class),
+            $linkCache,
+        );
+
+        $container->set(LinkRepository::class, $mockRepo);
+        $container->set(LinkService::class, $mockService);
+        $container->set(LinkController::class, new LinkController($mockService));
+
+        try {
+            $this->get("/cached")->assertStatus(302);
+            $this->get("/cached")->assertStatus(302);
+        } finally {
+            $linkCache->invalidate("cached");
+            $realRepo = $container->get(EloquentLinkRepository::class);
+            $realService = new LinkService(
+                $realRepo,
+                $container->get(SlugGenerator::class),
+                $linkCache,
+            );
+            $container->set(LinkRepository::class, $realRepo);
+            $container->set(LinkService::class, $realService);
+            $container->set(LinkController::class, new LinkController($realService));
+        }
     }
 }
