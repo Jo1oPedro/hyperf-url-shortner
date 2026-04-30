77a92ad80772a016d6f20a89b0af9572b227b558
Jo1o Pedro <joao.pedreira@estudante.ufjf.br>
2026-04-29 21:01:11 -0300

    feat(metrics): add metrics to the queue service

--- app/Service/QueueService.php
+++ app/Service/QueueService.php
@@ -2,6 +2,7 @@
 
 namespace App\Service;
 
+use App\Observability\Metrics;
 use Hyperf\AsyncQueue\Driver\DriverFactory;
 use Hyperf\AsyncQueue\JobInterface;
 use Hyperf\Cache\Driver\DriverInterface;
@@ -13,13 +14,21 @@ class QueueService
      */
     protected $driver;
 
-    public function __construct(DriverFactory $driverFactory)
-    {
+    public function __construct(
+        DriverFactory $driverFactory,
+        private readonly Metrics $metrics
+    ) {
         $this->driver = $driverFactory;
     }
 
     public function push(JobInterface $job, int $delay = 0): bool
     {
-        return $this->driver->get("default")->push($job, $delay);
+        $pushed = $this->driver->get("default")->push($job, $delay);
+
+        if($pushed) {
+            $this->metrics->queueJobPushed(basename(str_replace('\\', '/', $job::class)));
+        }
+
+        return $pushed;
     }
 }
